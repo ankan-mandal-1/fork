db2d73853696e3ecf8dd33eee6cb688694b6041f
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2025-01-20 08:03:28 +0530

    Create 6_sleep.c

---
+++ 6_sleep.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    printf("program to sleep for 10 seconds in Linux\n");
+    sleep(10);
+    printf("This line will be executed after 10 seconds");
+    return 0;
+}
