467a557c12d992deee882d729a2d9b40981d0fbf
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2025-01-20 08:01:50 +0530

    Update 5_wait.c

--- 5_wait.c
+++ 5_wait.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,4 +12,16 @@ int main() {
         printf("Child has terminated\n");
     }
     printf("Bye\n");
+    
+    if(child==0){
+        printf("Child: I am running!\n\n");
+        printf("Child: I have PID: %d\n", getpid());
+        sleep(4);
+        exit(100);
+    } else {
+        printf("Parent: I am running and waiting for child to finish");
+        childpid = wait(&exitstatus);
+        printf("Parent: child finish execution!, It had the PID: %d, exit status %d\n\n", childpid, WEXITSTATUS(exitstatus));
+    }
+    return 0;
 }
